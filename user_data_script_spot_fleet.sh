54b501ebc6c4c8d2a732aec04ed089cc5fb20a45
Kim <sjkim@f01898a3ba77.ant.amazon.com>
2019-07-16 03:00:04 +0900

    minor change

--- user_data_script_spot_fleet.sh
+++ user_data_script_spot_fleet.sh
@@ -7,12 +7,11 @@ AWS_REGION=ap-northeast-2
 # Make directories
 mkdir /dltraining
 chown -R ubuntu: /dltraining/
-cd /home/ubuntu/
 
 # Get training code
 # git clone https://github.com/go4real/ec2-spot-deep-learning.git
 # chown -R ubuntu: ec2-spot-deep-learning
-cd ec2-spot-deep-learning
+cd /home/ubuntu/ec2-spot-deep-learning
 
 # Initiate training using the tensorflow_36 conda environment
 sudo -H -u ubuntu bash -c "source /home/ubuntu/anaconda3/bin/activate tensorflow_p36; python ec2_spot_keras_training.py"
@@ -21,5 +20,5 @@ sudo -H -u ubuntu bash -c "source /home/ubuntu/anaconda3/bin/activate tensorflow
 
 
 # After training, clean up by cancelling spot requests and terminating itself
-SPOT_FLEET_REQUEST_ID=$(aws ec2 describe-spot-instance-requests --region $AWS_REGION --filter "Name=instance-id,Values='$INSTANCE_ID'" --query "SpotInstanceRequests[].Tags[?Key=='aws:ec2spot:fleet-request-id'].Value[]" --output text)
-aws ec2 cancel-spot-fleet-requests --region $AWS_REGION --spot-fleet-request-ids $SPOT_FLEET_REQUEST_ID --terminate-instances
+# SPOT_FLEET_REQUEST_ID=$(aws ec2 describe-spot-instance-requests --region $AWS_REGION --filter "Name=instance-id,Values='$INSTANCE_ID'" --query "SpotInstanceRequests[].Tags[?Key=='aws:ec2spot:fleet-request-id'].Value[]" --output text)
+# aws ec2 cancel-spot-fleet-requests --region $AWS_REGION --spot-fleet-request-ids $SPOT_FLEET_REQUEST_ID --terminate-instances
